0e2af41e6b1e2577ff7945b93f40729b01117516
zied444 <zied.elwakkel@esprit.tn>
2026-03-02 15:37:13 +0100

    second commit Angular project

--- src/app/details-res/details-res.component.ts
+++ src/app/details-res/details-res.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ResidenceService } from '../services/residence.service';
+import { Residence } from '../models/residence';
 
 @Component({
   selector: 'app-details-res',
   templateUrl: './details-res.component.html',
   styleUrl: './details-res.component.css'
 })
-export class DetailsResComponent {
+export class DetailsResComponent implements OnInit {
 
+  residence?: Residence;
+
+  constructor(
+    private route: ActivatedRoute,
+    private resService: ResidenceService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.resService.getres(id).subscribe((data: any) => {
+        this.residence = data;
+      });
+    }
+  }
 }
